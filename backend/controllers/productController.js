c560ee115db820a73a45d338d9a678ffaef144d3
Hamza <hamzanaseer711@gmail.com>
2022-02-03 15:45:01 +0500

    adding error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,64 +1,92 @@
 const Product = require("../models/productModel"); //importing product schema
 
+//TODO:ERROR HANDELING
+
 //create a product -- ADMIN
 exports.createProduct = async (req, res, next) => {
-  const product = await Product.create(req.body);
-  return res.status(201).json({ success: true, product: product });
+  try {
+    const product = await Product.create(req.body);
+    return res.status(201).json({ success: true, product: product });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
 };
 
 //get all products
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({ success: true, products });
+  try {
+    const products = await Product.find();
+    return res.status(200).json({ success: true, products });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
 };
 
 //updating a product --ADMIN
 exports.updateProduct = async (req, res, next) => {
-  //finding the product to be updated
-  let productToUpdate = await Product.findById(req.params.id);
-  //if wrong id then quit
-  if (!productToUpdate) {
-    return res
-      .status(500)
-      .json({ success: false, message: "product not found" });
+  try {
+    //finding the product to be updated
+    let productToUpdate = await Product.findById(req.params.id);
+    //if wrong id then quit
+    if (!productToUpdate) {
+      return res
+        .status(500)
+        .json({ success: false, message: "product not found" });
+    }
+    //product found
+    //now update it
+    //new:true will return us the updated product
+    productToUpdate = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({ success: true, productToUpdate });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ success: false, message: "internal server error" });
   }
-  //product found
-  //now update it
-  //new:true will return us the updated product
-  productToUpdate = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  return res.status(200).json({ success: true, productToUpdate });
 };
 //deleting a product --ADMIN
 exports.deleteProduct = async (req, res, next) => {
-  //finding the product to be deleted
-  let productToDelete = await Product.findById(req.params.id);
-  //if wrong id then quit
-  if (!productToDelete) {
+  try {
+    //finding the product to be deleted
+    //get product details
+    let productToDelete = await Product.findById(req.params.id);
+    //if wrong id then quit
+    if (!productToDelete) {
+      return res
+        .status(500)
+        .json({ success: false, message: "product not found" });
+    }
+    //product found
+    //now delete it
+    await Product.findByIdAndDelete(req.params.id);
     return res
-      .status(500)
-      .json({ success: false, message: "product not found" });
+      .status(200)
+      .json({ success: true, message: "product deleted successfully" });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ success: false, message: "internal server error" });
   }
-  //product found
-  //now delete it
-  await Product.findByIdAndDelete(req.params.id);
-  return res
-    .status(200)
-    .json({ success: true, message: "product deleted successfully" });
 };
-//get product details
+
 exports.getProductDetails = async (req, res, next) => {
-  //finding the product to be deleted
-  let product = await Product.findById(req.params.id);
-  //if wrong id then quit
-  if (!product) {
-    return res
-      .status(500)
-      .json({ success: false, message: "product not found" });
+  try {
+    //finding the product to get the details
+    let product = await Product.findById(req.params.id);
+    //if wrong id then quit
+    if (!product) {
+      return res
+        .status(500)
+        .json({ success: false, message: "product not found" });
+    }
+    //product found
+    //now send details
+    return res.status(200).json({ success: true, product });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ success: false, message: "internal server error" });
   }
-  //product found
-  //now send details
-  return res.status(200).json({ success: true, product });
 };
