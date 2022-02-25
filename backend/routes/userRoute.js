9f127f1c5a10816aa23c9778750ddf24f47deab6
Hamza <hamzanaseer711@gmail.com>
2022-02-25 16:48:46 +0500

    testing git

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser, login, logout } = require("../controllers/userController");
+const { createUser, login, logout, forgotPassword } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/createuser").post(createUser);
 router.post("/login", login);
 router.get("/logout", logout)
+router.post("/password/forgot",forgotPassword)
 module.exports = router;
