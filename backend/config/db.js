a9702658f219d992844e784b9dc43712f40ba31f
Hamza <hamzanaseer711@gmail.com>
2022-02-25 16:57:42 +0500

    Remove the now ignored directory "config"

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectToMongo = () => {
-  mongoose.connect(process.env.DB_URI, () => {
-    console.log("connected to mongoose sucessfully");
-  });
-};
-
-module.exports = connectToMongo;
