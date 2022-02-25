ca997aafab9a3063abee53b04e58b8841889fb5e
Hamza <hamzanaseer711@gmail.com>
2022-02-25 16:34:04 +0500

    updated gitignore

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,1 +1,20 @@
-//TODO ::: create send email function
+const nodeMailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      password: process.env.SMTP_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
