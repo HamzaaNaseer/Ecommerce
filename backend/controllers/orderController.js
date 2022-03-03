89b8dccb87fc743e05b1f2788ae43d3ba9997968
Hamza <hamzanaseer711@gmail.com>
2022-03-04 02:39:01 +0500

    adding admin routes to the order (update,delete,getall)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,5 @@
 const Order = require("../models/orderModel"); //importing order schema
+const Product = require("../models/productModel"); //importing product schema
 
 //creating order route -- requires login
 exports.createOrder = async (req, res) => {
@@ -38,7 +39,10 @@ exports.createOrder = async (req, res) => {
 //route for getting an order with id
 exports.getOrder = async (req, res) => {
   try {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
     if (!order) {
       return res
         .status(500)
@@ -47,13 +51,12 @@ exports.getOrder = async (req, res) => {
     //order found here
     return res.status(201).json({ success: true, order });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, message: "internal server error" });
+    console.log("error is ", error);
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
 
-//route for getting orders -- login required
+//route for getting loged in user orders -- login required
 exports.myOrders = async (req, res) => {
   try {
     const orders = await Order.find({ user: req.user._id });
@@ -63,5 +66,77 @@ exports.myOrders = async (req, res) => {
     return res
       .status(500)
       .json({ success: false, message: "internal server error" });
+  }
+};
+
+//route for getting all orders -- ADMIN
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    //counting the sum of money of all the orders
+    //just counting so the admin should know
+    let totalPrice = 0;
+    orders.forEach((order) => {
+      totalPrice += order.totalPrice;
+    });
+    return res.status(201).json({ success: true, totalPrice, orders });
+  } catch (error) {
+    console.log("error is ", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//route for updating  order  -- ADMIN
+exports.updateOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res
+        .status(500)
+        .json({ success: false, message: "No order found with provided id" });
+    }
+    //if the order is already delivered it can't be updated
+    if (order.orderStatus === "Delivered") {
+      return res
+        .status(400)
+        .json({ message: "you have already delivered this order" });
+    }
+    order.orderItems.forEach(async (items) => {
+      //updating stock in the database
+      await updateStock(items.product, items.quantity);
+    });
+    order.orderStatus = req.body.status;
+    if (req.body.status === "Delivered") {
+      order.deliveredAt = Date.now();
+    }
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log("error is ", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+async function updateStock(prodId, quantity) {
+  const product = await Product.findById(prodId);
+  //minus the order quantity from product stock
+  product.stock -= quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+//route for deleting order -- ADMIN
+exports.deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res
+        .status(500)
+        .json({ success: false, message: "No order found with provided id" });
+    }
+    await order.remove();
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log("error is ", error);
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,12 +3,28 @@ const {
   createOrder,
   myOrders,
   getOrder,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
-const { isAuthenticated } = require("../middleware/isAuthenticated");
+const {
+  isAuthenticated,
+  authorizeRoles,
+} = require("../middleware/isAuthenticated");
 const router = express.Router();
 
 router.post("/order/new", isAuthenticated, createOrder);
 router.get("/order/:id", getOrder);
-router.get("/order/myOrders", isAuthenticated, myOrders);
+router.get("/orders/me", isAuthenticated, myOrders);
+router.get(
+  "/admin/orders",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticated, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticated, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
